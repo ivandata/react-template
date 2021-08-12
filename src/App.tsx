8174f6358a8f545534f2e5cf3bce468250c51fbf
Ivandata <ivandata@users.noreply.github.com>
2021-08-12 11:24:57 +0200

    feat: add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import mainTheme from 'theme/theme';
 import { SPageHeader, SPageContainer, SLink } from './App.styles';
 
 const App: React.FunctionComponent = () => {
   return (
-    <SPageContainer>
-      <SPageHeader>
-        <SLink
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </SLink>
-      </SPageHeader>
-    </SPageContainer>
+    <ThemeProvider theme={mainTheme}>
+      <SPageContainer>
+        <SPageHeader>
+          <SLink
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            Learn React
+          </SLink>
+        </SPageHeader>
+      </SPageContainer>
+    </ThemeProvider>
   );
 };
 
