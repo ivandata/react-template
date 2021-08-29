28e7cb73d1c5f0b322ed8c2d4a71480dda1ec557
Ivandata <ivandata@users.noreply.github.com>
2021-08-29 18:33:42 +0200

    feat: add font, and theme tokens

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,20 +1,54 @@
 import styled from 'styled-components';
 
 export const SPageContainer = styled.div`
-  text-align: center;
+  max-height: 100%;
+  height: 100%;
+  color: ${({ theme }) => theme.colors.txt.body};
+  background-color: ${({ theme }) => theme.colors.bkg.page};
 `;
 
 export const SPageHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
+  padding: ${({ theme }) => theme.spacing.lg}rem
+    ${({ theme }) => theme.spacing.sm}rem;
   display: flex;
-  flex-direction: column;
-  align-items: center;
   justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
 `;
 
-export const SLink = styled.a`
-  color: #61dafb;
+export const SHeaderLogo = styled.div`
+  width: 50px;
+
+  svg {
+    width: 100%;
+  }
+`;
+
+export const SPageContent = styled.main`
+  max-height: 100%;
+  max-width: 600px;
+  padding: ${({ theme }) => theme.spacing.md}rem;
+  margin: 0 auto;
+
+  h1 {
+    font-size: ${({ theme }) => theme.font.size.h1}rem;
+  }
+
+  h2 {
+    font-size: ${({ theme }) => theme.font.size.h2}rem;
+  }
+
+  h3 {
+    font-size: ${({ theme }) => theme.font.size.h3}rem;
+  }
+
+  h3 {
+    font-size: ${({ theme }) => theme.font.size.h4}rem;
+  }
+
+  & * {
+    transition-property: background-color, border-color, color, fill, stroke,
+      opacity, box-shadow, transform, filter, backdrop-filter, -webkit-filter,
+      -webkit-backdrop-filter;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 0.15s;
+  }
 `;
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,19 +1,44 @@
 export interface MainTheme {
   colors: {
     bkg: {
-      [key: string]: string
-    }
-  },
+      [key: string]: string;
+    };
+    txt: {
+      [key: string]: string;
+    };
+    interactive: {
+      primary: {
+        [key: string]: string;
+      };
+    };
+  };
   spacing: {
-    [key: string]: number
-  }
+    [key: string]: number;
+  };
+  font: {
+    size: {
+      [key: string]: number;
+    };
+  };
 }
 
 export const mainTheme = {
   colors: {
     bkg: {
       page: '#F3F4F6',
-      container: '#F9FAFB'
+      container: '#F9FAFB',
+    },
+    txt: {
+      body: '#1F2937',
+      description: '#9CA3AF',
+    },
+    interactive: {
+      primary: {
+        default: '#fff',
+        hover: '#3B82F6',
+        active: '#60A5FA',
+        disabled: '#D1D5DB',
+      },
     },
   },
   spacing: {
@@ -21,8 +46,16 @@ export const mainTheme = {
     sm: 0.5,
     md: 1,
     lg: 1.5,
-    xl: 2
-  }
+    xl: 2,
+  },
+  font: {
+    size: {
+      h1: 2.5,
+      h2: 2,
+      h3: 1.8,
+      h4: 1.3,
+    },
+  },
 };
 
 export default mainTheme;
